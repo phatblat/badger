e6e9dab71e35a4073a3e8a831727f82bc86684ab
Ben Chatelain <ben@octop.ad>
2018-03-30 17:13:15 -0600

    🏛 Add BadgeKit target

--- Package.swift
+++ Package.swift
@@ -6,23 +6,28 @@ import PackageDescription
 let package = Package(
     name: "badger",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
+        .executable(
             name: "badger",
             targets: ["badger"]),
+        .library(
+            name: "BadgeKit",
+            targets: ["BadgeKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "BadgeKit",
+            dependencies: [],
+            path: "Sources/BadgeKit"),
         .target(
             name: "badger",
-            dependencies: []),
+            dependencies: [.target(name: "BadgeKit")],
+            path: "Sources/badger"),
         .testTarget(
             name: "badgerTests",
-            dependencies: ["badger"]),
+            dependencies: [.target(name: "BadgeKit")]),
     ]
 )
