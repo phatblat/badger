99a9938e345d5214a6d4905e58a2754300f57eed
Ben Chatelain <ben@octop.ad>
2018-03-30 16:11:33 -0600

    ➕🔌 Add SwiftPM plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,28 @@
  */
 
 rootProject.name = "badger"
+
+val swiftPM = file("../../gradle/SwiftPM-Plugin")
+if (swiftPM.exists()) {
+    // Include SwiftPM plugin project in composite build
+    includeBuild(swiftPM) {
+        dependencySubstitution {
+            substitute(module("at.phatbl:swiftpm")).with(project(":"))
+        }
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "at.phatbl.swiftpm" ->
+                    useModule("at.phatbl:swiftpm:${requested.version}")
+                else -> Unit
+            }
+        }
+    }
+}
